f787cc4a4405f742d142ec32a2e0dcfe386b73a4
LuizGustavo17 <43247389+LuizGustavo17@users.noreply.github.com>
2023-11-02 00:29:31 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+$('#campo_telefone').mask('(00) 00000-0000', {placeholder: '(__) _____-____'});
+$('#campo_cpf').mask('000.000.000-00', {placeholder: '___.___.___-__'});
+$('#campo_cep').mask('00.000-000', {placeholder: '__.___-___'});
+});
